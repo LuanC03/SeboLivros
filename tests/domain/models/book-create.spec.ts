d8b92c8dc1ffc350267fd051cacde62cec31e08d
Luan Carlos <luan.bezerra@ccc.ufcg.edu.br>
2022-03-14 02:36:10 -0300

    test: adicionando testes nos modelos de institution e book, para aumentar o coverage

--- tests/domain/models/book-create.spec.ts
+++ tests/domain/models/book-create.spec.ts
@@ -1,4 +1,5 @@
 import { BookService } from '@/data/services'
+import { Book } from '@/domain/models'
 
 describe('Book-Create', () => {
   function makeSut (): BookService {
@@ -16,8 +17,36 @@ describe('Book-Create', () => {
     })
   }
 
-  it('testando a criação do livro via parametros', async () => {
+  it('testando a criação do livro via parametros no service', async () => {
     const sut = makeSut()
+
+    expect(sut).toEqual({
+      id: sut.id,
+      name: 'Livro1',
+      edition: 3,
+      releaseYear: 2012,
+      releaseDate: new Date(21 / 10 / 2012),
+      conservationStateUsed: 10,
+      conservationStateNew: 13,
+      conservationStateDamaged: 2,
+      libraryId: 'any_Id',
+      institutionAddress: 'any_address'
+    })
+  })
+
+  it('testando a criação do livro via parametros, no modelo', async () => {
+    const sut = new Book({
+      id: '123',
+      name: 'Livro1',
+      edition: 3,
+      releaseYear: 2012,
+      releaseDate: new Date(21 / 10 / 2012),
+      conservationStateUsed: 10,
+      conservationStateNew: 13,
+      conservationStateDamaged: 2,
+      libraryId: 'any_Id',
+      institutionAddress: 'any_address'
+    })
 
     expect(sut).toEqual({
       id: sut.id,
--- tests/domain/models/institution-create.spec.ts
+++ tests/domain/models/institution-create.spec.ts
@@ -1,5 +1,6 @@
 import { CreateInstitutionApi } from '@/data/contracts/apis'
 import { InstitutionService } from '@/data/services'
+import { Institution } from '@/domain/models'
 
 class CreateInstitutionApiSpy implements CreateInstitutionApi {
   params?: CreateInstitutionApi.Params
@@ -8,7 +9,7 @@ class CreateInstitutionApiSpy implements CreateInstitutionApi {
   }
 }
 describe('InstitutionService', () => {
-  it('', async () => {
+  it('Testando a criação de uma instituição via service', async () => {
     const createInstitutionApi = new CreateInstitutionApiSpy()
     const sut = new InstitutionService(createInstitutionApi)
 
@@ -29,6 +30,37 @@ describe('InstitutionService', () => {
     expect(createInstitutionApi.params).toStrictEqual({
       name: 'any_name',
       address: {
+        street: 'any_street',
+        number: 'any_number',
+        district: 'any_district',
+        city: 'any_city',
+        uf: 'any_uf',
+        complement: 'any_complement',
+        cep: 123
+      },
+      admId: 'any_id'
+    })
+  })
+  it('Testando a criação de uma instituição via model', async () => {
+    const sut = new Institution({
+      id: '123',
+      name: 'any_name',
+      institutionAddress: {
+        street: 'any_street',
+        number: 'any_number',
+        district: 'any_district',
+        city: 'any_city',
+        uf: 'any_uf',
+        complement: 'any_complement',
+        cep: 123
+      },
+      admId: 'any_id'
+    })
+
+    expect(sut).toEqual({
+      id: sut.id,
+      name: 'any_name',
+      institutionAddress: {
         street: 'any_street',
         number: 'any_number',
         district: 'any_district',
