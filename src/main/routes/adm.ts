4379818e9c69f1dd6ebe07ca24ae3c7e54221b86
Luan Carlos <luan.bezerra@ccc.ufcg.edu.br>
2022-04-12 17:21:52 -0300

    fix: consertando criação do adm

--- src/main/routes/adm.ts
+++ src/main/routes/adm.ts
@@ -1,8 +1,12 @@
-
-import { Router } from 'express'
+import { Request, Response, Router } from 'express'
+import { prismaClient } from '@/data/contracts/repos'
 
 export default (router: Router): void => {
-  router.post('/api/adm', (req, res) => {
-    res.send({ response: 'saida' })
+  router.post('/api/adm', (req: Request, res: Response) => {
+    const { name, email, username, password } = req.body
+    const adm = prismaClient.administrator.create({
+      data: { name, email, username, password }
+    })
+    res.status(201).send(adm)
   })
 }
