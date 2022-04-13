6714f9e3ee2185d0ea3899a37bc2956d7633385f
Luan Carlos <luan.bezerra@ccc.ufcg.edu.br>
2022-04-13 12:29:13 -0300

    refactor: atualizando imports após as refatorações

--- src/data/contracts/repos/adm-account-repo.ts
+++ src/data/contracts/repos/adm-account-repo.ts
@@ -1,6 +1,3 @@
-/* import { AccessToken } from '@/domain/models'
-import { AuthenticationError } from '@/domain/errors' */
-
 export interface LoadAdmAccountRepository {
   loadAdm: (params: LoadAdmAccountRepository.Params) => Promise<LoadAdmAccountRepository.ResultAdm>
 }
--- src/domain/errors/index.ts
+++ src/domain/errors/index.ts
@@ -1,2 +1,3 @@
-export * from './authentication-error'
+export * from './authentication-adm-error'
 export * from './update-book-error'
+export * from './creation-adm-error'
