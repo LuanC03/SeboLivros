84c9c7b9c5ac3a85ff3537a3ce0e7fe8576174e0
Luan Carlos <luan.bezerra@ccc.ufcg.edu.br>
2022-04-15 16:20:35 -0300

    feat: adicionando metodos de CRUD adm

--- src/main/routes/adm-route.ts
+++ src/main/routes/adm-route.ts
@@ -1,12 +1,40 @@
 import { Request, Response, Router } from 'express'
 import { prismaClient } from '@/data/contracts/repos'
+/* import { AdminCreateController } from '@/application/controllers' */
 
 export default (router: Router): void => {
-  router.post('/api/adm', (req: Request, res: Response) => {
+  router.post('/api/adm/', /* new AdminCreateController().handle) */ async (req: Request, res: Response) => {
     const { name, email, username, password } = req.body
-    const adm = prismaClient.administrator.create({
-      data: { name, email, username, password }
-    })
-    res.status(201).send(adm)
+    console.log(name)
+    try {
+      const adm = await prismaClient.administrator.create({
+        data: { name: name, email: email, username: username, password: password }
+      })
+      res.status(201).send(adm)
+    } catch (error) {
+      res.status(400).send(error)
+    }
+  })
+  router.get('/api/adm/', /* new AdminCreateController().handle) */async (req: Request, res: Response) => {
+    const { name, email, username, password } = req.body
+    console.log(name)
+    try {
+      const adm = await prismaClient.administrator.findFirst({ where: { name, email, username, password } })
+      console.log(adm)
+      res.status(302).send(adm)
+    } catch (error) {
+      res.status(404).send(error)
+    }
+  })
+  router.delete('/api/adm/', /* new AdminCreateController().handle) */async (req: Request, res: Response) => {
+    const { name, email, username, password } = req.body
+    console.log(name)
+    try {
+      const adm = await prismaClient.administrator.findFirst({ where: { name, email, username, password } })
+      await prismaClient.administrator.delete({ where: { id: adm?.id } })
+      res.status(204).send(adm)
+    } catch (error) {
+      res.status(404).send(error)
+    }
   })
 }
