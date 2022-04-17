56e8b7bcd93c5abc2d5b7cb55a3129c7e59c9bc7
Luan Carlos <luan.bezerra@ccc.ufcg.edu.br>
2022-04-17 19:26:49 -0300

    refactor: organizando e preparando para nova feat

--- src/data/services/adm-service.ts
+++ src/data/services/adm-service.ts
@@ -1,18 +1,19 @@
 import { AuthenticationAdmError, CreationAdmError } from '@/domain/errors'
 import { TokenGenerator } from '@/data/contracts/crypto/token'
-import { UpdateAdmAccountRepository, LoadAdmAccountRepository, CreateAdmAccountRepository, prismaClient } from '@/data/contracts/repos'
+import { UpdateAdmAccountRepository, LoadAdmAccountRepository, CreateAdmAccountRepository, DeleteAdmAccountRepository, prismaClient } from '@/data/contracts/repos'
 import { AccessToken } from '@/domain/models'
 import { Administrator } from '@prisma/client'
 
 export class AdmService {
   constructor (
-    private readonly AdmAccountRepo: LoadAdmAccountRepository & CreateAdmAccountRepository & UpdateAdmAccountRepository,
+    private readonly AdmAccountRepo:
+    LoadAdmAccountRepository & CreateAdmAccountRepository &
+    DeleteAdmAccountRepository & UpdateAdmAccountRepository,
     private readonly crypto: TokenGenerator
   ) { }
 
   async load (params: LoadAdmAccountRepository.Params): Promise<Administrator | AuthenticationAdmError> {
     const loadUser = await prismaClient.administrator.findFirst({ where: { username: params.username } })/* await this.AdmAccountRepo.loadAdm(params) */
-    /* console.log(loadUser?.email) */
     if (loadUser != null) {
       if (params.password === loadUser.password) {
         await this.crypto.generateToken({ token: loadUser.email, expirationInMs: AccessToken.expirationInMs })
@@ -22,19 +23,55 @@ export class AdmService {
     return new AuthenticationAdmError()
   }
 
-  async create (params: CreateAdmAccountRepository.Params): Promise<String | CreationAdmError> {
+  async create (params: CreateAdmAccountRepository.Params): Promise<string | CreationAdmError> {
     try {
-      await prismaClient.administrator.create({
-        data: {
-          email: params.email,
-          name: params.name,
-          password: params.password,
-          username: params.username
-        }
-      })
-      return 'ADM Criado'
-    } catch {
+      if (validaParametrosCriacao(params)) {
+        const adm = await prismaClient.administrator.create({
+          data: {
+            email: params.email,
+            name: params.name,
+            password: params.password,
+            username: params.username
+          }
+        })
+        return adm.email
+      } else {
+        throw new CreationAdmError('Parametros invalidos')
+      }
+    } catch (err) {
       return new CreationAdmError('Erro na criação')
     }
   }
+
+  /* async delete (params: DeleteAdmAccountRepository.Params): Promise<string | DeleteAdmError> {
+        try {
+      if (validaParametrosDelete(params)) {
+
+        await prismaClient.administrator.delete({
+          where: {
+            email: params.email
+          }
+        })
+        return 'ADM Deletado'
+      } else {
+
+        throw new DeleteAdmError('id invalido')
+      }
+    } catch (err) {
+      return new DeleteAdmError('Erro no delete')
+    }
+  } */
 }
+function validaParametrosCriacao (params: CreateAdmAccountRepository.Params): boolean {
+  let result = true
+  if (params.email === null || params.email === '') result = false
+  else if (params.name === null || params.name === '') result = false
+  else if (params.password === null || params.password === '') result = false
+  else if (params.username === null || params.username === '') result = false
+  return result
+}
+/* function validaParametrosDelete (params: DeleteAdmAccountRepository.Params): boolean {
+  let result = true
+  if (params.email === null || params.email === '') result = false
+  return result
+} */
--- src/main/routes/adm-route.ts
+++ src/main/routes/adm-route.ts
@@ -7,10 +7,8 @@ export default (router: Router): void => {
 
   router.get('/api/adm/', /* new AdminCreateController().handle) */async (req: Request, res: Response) => {
     const { name, email, username, password } = req.body
-    console.log(name)
     try {
       const adm = await prismaClient.administrator.findFirst({ where: { name, email, username, password } })
-      console.log(adm)
       res.status(302).send(adm)
     } catch (error) {
       res.status(404).send(error)
@@ -18,7 +16,6 @@ export default (router: Router): void => {
   })
   router.delete('/api/adm/', /* new AdminCreateController().handle) */async (req: Request, res: Response) => {
     const { name, email, username, password } = req.body
-    console.log(name)
     try {
       const adm = await prismaClient.administrator.findFirst({ where: { name, email, username, password } })
       await prismaClient.administrator.delete({ where: { id: adm?.id } })
